9899026667be22877295495b50551c10aaba19c6
icvitkov <ivana.cvit@hotmail.com>
2023-11-06 11:12:32 +0100

    add primevue ui kit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { registerSW } from 'virtual:pwa-register'
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/soho-dark/theme.css'
+
+
 
 const upateSW = registerSW({
     onNeedRefresh() {},
@@ -14,5 +19,5 @@ const upateSW = registerSW({
 const app = createApp(App)
 
 app.use(router)
-
+app.use(PrimeVue);
 app.mount('#app')
