ccffaad5f68d2f5a652759e02a05d9c54fb3ecca
icvitkov <cvitkov.ivana@gmail.com>
2023-11-23 15:59:42 +0100

    edit style

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { registerSW } from 'virtual:pwa-register'
-import PrimeVue from 'primevue/config';
+import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/soho-dark/theme.css'
 
-
 const upateSW = registerSW({
-    onNeedRefresh() {},
-    onOfflineReady() {},
+  onNeedRefresh() {},
+  onOfflineReady() {}
 })
-
 
 const app = createApp(App)
 const pinia = createPinia()
